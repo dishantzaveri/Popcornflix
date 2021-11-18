7f61c237d8d1f8c79f1d7e91cfd84c56906f7f14
dishantzaveri <zaveridishant@gmail.com>
2021-11-18 22:15:07 +0530

    splash screen

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React , {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Providers from './navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 function App() {
+  const [hideSplash, setHideSplash] = useState(false);
+
+useEffect(() => {
+  setTimeout(() => {
+    setHideSplash(true);
+  }, 1000); // amount of time the splash is shown from the time component is rendered
+}, []);
+
+useEffect(() => {
+  hideSplash && SplashScreen.hide();
+}, [hideSplash]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Providers">
--- android/app/src/main/java/com/task1lp/MainApplication.java
+++ android/app/src/main/java/com/task1lp/MainApplication.java
@@ -13,6 +13,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -42,6 +43,8 @@ public class MainApplication extends Application implements ReactApplication {
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
+
+ 
 
   @Override
   public void onCreate() {
